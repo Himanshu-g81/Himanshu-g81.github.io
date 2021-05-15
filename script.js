3fa8599b6abced20509779e4a3c29e4cfc0dd37c
Himanshu <hgwalani81@gmail.com>
2021-05-15 21:45:52 +0530

    Initial website

---
+++ script.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    
+    $(window).scroll(function() {
+
+        if(this.scrollY > 20) {
+            $('.navbar').addClass('sticky');
+        }
+        else {
+            $('.navbar').removeClass('sticky');
+        }
+
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        }
+        else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    })
+
+    // slide up
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        })
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ['Software Developer', 'Competitive Programmar', 'Guy who try to Rap'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    })
+
+    // typing animation script
+    var typed_1 = new Typed(".typing-1", {
+        strings: ['Software Developer', 'Competitive Programmar', 'Guy who try to Rap'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    })
+
+    $('.menu-btn').click(function() {
+        console.log("clicked");
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    })
+
+    // owl carousel script
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        
+        autoplay: true,
+        // autolayTimeOut: 2000,
+        autoplayHoverPauseon: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+
+            600: {
+                items: 2,
+                nav: false
+            },
+
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+
+    })
+
+    
+
+    const form = document.querySelector('form')
+    form.addEventListener('submit', event => {
+
+        event.preventDefault()
+        // submit event detected
+        const {name, email, subject, message} = event.target.elements;
+        
+        
+    })
+})
